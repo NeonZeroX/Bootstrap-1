de8cc824db1eee30c9302e99345da9dcd1cd248c
NeonZeroX <80101013+NeonZeroX@users.noreply.github.com>
2021-12-09 06:30:17 +0000

    Create Bootstrap 1.php

---
+++ Bootstrap 1.php
@@ -0,0 +1,183 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>63118343</title>
+</head>
+<style>
+    .block-1 {
+        background-color: red;
+    }
+
+    .block-2 {
+        background-color: green;
+    }
+
+    .block-3 {
+        background-color: rgb(0, 0, 0);
+        width: 200px;
+        height: 1685px;
+        float: left;
+    }
+
+    .block-4 {
+        background-color: rgb(120, 253, 191);
+        text-align: center;
+        width: 80%
+    }
+    .txt {
+        color: seashell;
+    }
+</style>
+
+<body>
+    <div class="container-fluid">
+        <img src="https://i.pinimg.com/originals/b2/31/f2/b231f21955181bb8025b65c74a395d39.jpg"
+            class="img-fluid" alt="..." width="100%">
+        <div class="container">
+            <div class="col-12 block-3">
+            <a href="**" class="w3-bar-item w3-button txt">กำลังสร้าง</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://i.pinimg.com/736x/28/14/08/28140844e3bac703dcdb23b731c0793d.jpg"
+                        class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">ไซตามะเป็นฮีโร่ที่แข็งแกร่งที่สุดในโลก ในทุกๆครั้ง เขาจะสามารถสยบคู่ต่อสู้ได้ภายในหมัดเดียว เขาเป็นผู้ชายอายุ 25 มีหัวโล้น เนื่องจากการฝึกแสนหฤโหดเพื่อที่จะได้มาเป็นฮีโร่ ไซตามะเป็นคนที่รักความยุติธรรม เขาได้ช่วยคนบริสุทธิ์จากเหล่าสิ่งมีชีวิตประหลาด และช่วยโลกมาแล้วนับครั้งไม่ถ้วน แต่เขาไม่เคยได้รับผลตอบแทนสักนิดเดียวเดิมทีแล้วไซตามะเป็นแค่คนธรรมดาที่หางานทำ จนกระทั่งได้ไปช่วยเหลือเด็กจากสัตว์ประหลาด ตั้งแต่นั้นมาไซตามะก็มีความพยายามที่จะเป็นฮีโร่โดยการ กินกล้วยเป็นอาหารเช้า ไม่เปิดแอร์หน้าร้อน และ ไม่เปิดฮีตเตอร์ในหน้าหนาว ซิทอัพ 100 ครั้ง วิดพื้น 100 ครั้ง ลุกนั่ง 100 ครั้ง และวิ่ง 10 กม.ทุกวัน เป็นระยะเวลา 3 ปี ทำให้เขาได้พลังเหนือมนุษย์มา แต่ก็สูญเสียเส้นผมไปจนหมด</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://wallpapers.com/images/featured/killua-raxgvyny51ylo7wl.jpg"
+                        class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">เคิรัวร์ โซลดิ๊ก  เกิด 7 กรกฎาคม เด็กหนุ่มในตระกูลนักฆ่าโซลดิ๊กที่มีชื่อเสียงเรื่องการลอบสังหาร ถูกเลี้ยงดูมาอย่างนักฆ่าตั้งแต่เล็ก ฝีมือการลอบสังหารเข้าขั้นอัจฉริยะจึงถูกครอบครัวคาดหวังไว้มาก แต่เจ้าตัวอยากใช้ชีวิตในแบบของตน จึงเข้าสมัครสอบฮันเตอร์และได้พบกับกอร์น คุราปิก้าและเลโอลีโอจนเป็นเพื่อนสนิทกัน อุปนิสัยโลเลของคิรัวร์ที่ผ่านมาเป็นเพราะถูกอิรุมิฝังเข็มเน็นไว้เพื่อบัง คับให้คิรัวร์หลีกหนีจากอันตรายโดยอัตโนมัติ หลังจากถอนเข็มออกในภาคคิเมร่าแอ๊นท์จึงมีความกล้าที่จะต่อสู้เสี่ยงตายได้ อีกครั้ง ถึงกระนั้นคิรัวร์ยังไม่สามารถตัดใจลงมือฆ่าคนในครอบครัวได้ 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-9">
+                <button id="btnBack"> back </button>
+
+                <div id="main">
+                    <table  class="table table-success table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Author</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tblPost">
+                        </tbody>
+                    </table>
+                </div>
+
+                <div id="detail">
+                    <table class="table table-success table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>UserID</th>
+                            </tr>
+                        </thead>
+                        <tbody id="tblDetails">
+                        </tbody>
+                    </table>
+                </div>
+
+
+            </div>
+        </div>
+        </div>
+    </div>
+    <footer class="w3-container w3-padding-64 w3-light-grey w3-center w3-large"> 
+        <i class="fa fa-facebook-official w3-hover-opacity"></i>
+        <i class="fa fa-instagram w3-hover-opacity"></i>
+        <i class="fa fa-snapchat w3-hover-opacity"></i>
+        <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+        <i class="fa fa-twitter w3-hover-opacity"></i>
+        <i class="fa fa-linkedin w3-hover-opacity"></i>
+        <h1>63118343 จิรวัฒน์ ชาตรี </h1>
+    </footer>
+</body>
+<script>
+    function showDetails(id) {
+        $("#main").hide();
+        $("#detail").show();
+
+        var url = "https://jsonplaceholder.typicode.com/posts/" + id
+
+        $.getJSON(url)
+            .done((data) => {
+                console.log(data);
+                var line = "<tr id='detailROW'";
+                line += "><td>" + data.id + "</td>"
+                line += "<td><b>" + data.title + "</b><br/>"
+                line += data.body + "</td>"
+                line += "<td>" + data.userId + "</td>"
+                line += "</tr>";
+                $("#tblDetails").append(line);
+            })
+            .fail((xhr, err, status) => {
+
+            })
+
+
+    }
+
+    function LoadPosts() {
+        var url = "https://jsonplaceholder.typicode.com/posts"
+        var i = 0;
+        $.getJSON(url)
+            .done((data) => {
+                $.each(data, (k, item) => {
+                    i++;
+                    var line = "<tr>";
+                    line += "<td>" + item.id + "</td>"
+                    line += "<td><b>" + item.title + "</b><br/>"
+                    line += item.body + "</td>"
+                    line += "<td><button onClick='showDetails(" + item.id + ");'>Link</button></td>"
+                    line += "</tr>";
+                    $("#tblPost").append(line);
+                    if (i == 10) {
+                        loadPost().stop();
+                    };
+                });
+                $("#main").show();
+            })
+            .fail((xhr, err, status) => {
+
+            })
+    }
+
+    $(() => {
+        LoadPosts();
+        $("#detail").hide();
+        $("#btnBack").click(() => {
+            $("#main").show();
+            $("#detail").hide();
+            $("#detailROW").remove();
+        });
+    })
+</script>
+
+
+</html>
